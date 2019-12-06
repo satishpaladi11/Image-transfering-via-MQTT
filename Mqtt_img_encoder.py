18667cf1997859e9869d25dde8da5a4cc70ab999
SamSathish <satishpaladi11@gmail.com>
2019-12-06 14:49:05 +0530

    Update Mqtt_img_encoder.py
    Created comments for explanation

--- Mqtt_img_encoder.py
+++ Mqtt_img_encoder.py
@@ -1,28 +1,24 @@
+#For MQTT connection
 import paho.mqtt.client as mqtt
+#For encoding the image to ASCII format
 import base64
-import cv2
-#cap=cv2.VideoCapture(1)
+
+#Function for converting the image file to Base64 format
 def convertImageToBase64(img):
     with open(img, "rb") as image_file:
         encoded = base64.b64encode(image_file.read())
         return encoded
 
     
+#Setting up the MQTT connection
+#Changing the broker to your mqtt broker ipaddress at YOUR_MQTT_BROKER
 mqtt.Client.connected_flag=False #create flag in class
-broker="192.168.29.124"
+broker="YOUR_MQTT_BROKER ex:(192.168.29.147)"
 client = mqtt.Client()   
 client.connect(broker,1883,60)
-while True:
-    client.publish("sam",convertImageToBase64("photo.jpg"))
 
 
-'''
-while True:
-    rect, frame = cap.read()
-    retval, buffer = cv2.imencode('.jpg', frame)
-'''    
-    
-'''    
-cap.release
-cv2.destroyAllWindows()
-'''
+#You can change your topic at TOPIC
+#Change your "photo.jpg" to your location of image
+client.publish("TOPIC",convertImageToBase64("photo.jpg"))
+
